c4570c34004dbb3b00f6923197f937a2b204dde0
pasindu-sandeepa <pasindusandeep23@gmail.com>
2025-04-19 09:42:17 +0530

    first commiit

---
+++ src/main/java/com/JWT_Springboot_Security/JWT_Springboot_Security/JwtSpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.JWT_Springboot_Security.JWT_Springboot_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JWT_Springboot_Security/JWT_Springboot_Security/JwtSpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JWT_Springboot_Security.JWT_Springboot_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
